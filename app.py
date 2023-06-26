2c0a6be86cb7ac646981dfd3480dc2e942db54c8
Rafael Bressan <rfbressan@gmail.com>
2023-06-26 16:13:00 -0300

    fix: rename config.env

--- app.py
+++ app.py
@@ -3,6 +3,13 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+from langchain.llms import OpenAI
 
-print(os.getenv("OPENAI_API_KEY"))
+load_dotenv()
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(temperature=0.2)
+
+llm.predict(
+    "What would be a good company name for a company that makes colorful socks?"
+)
